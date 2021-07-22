910d7c7899bc2f8290a736f95eff3dbdbc4f3396
wallace <kollen22@outlook.com>
2021-07-22 17:15:38 -0300

    feat: change photos

--- src/config/portfolio.ts
+++ src/config/portfolio.ts
@@ -6,7 +6,7 @@ export const portfolio = [
         name: "SonhoVeg",
         description: "Este é um site de vendas de doces, onde há pagamentos, contas, painel de controle para administrar praticamente tudo que acontece no site, status das comidas e agendamento para entrega e muito mais. Este é um dos sites mais completos que já fiz, ainda não está totalmente completo, mas as principais funcionalidades está tudo funcionando muito bem.",
         link: "https://sonhoveg.com/",
-        image: "/sonhoveg.png",
+        image: "/sonhoveg.svg",
         reverse: false,
     },
     {
@@ -15,7 +15,7 @@ export const portfolio = [
         name: "Site Portfólio",
         link: "#",
         description: "Um site para mostrar meus trabalhos e contar um pouco sobre minha trajetória nesse mundo da programação.",
-        image: "/port.png",
+        image: "/port.svg",
         reverse: true,
     },
     {
@@ -24,7 +24,7 @@ export const portfolio = [
         name: "Site de vendas de sprites",
         description: "Este site é um site de vendas de sprites para jogos, possui um painel de controle muito completo para gerenciar tudo que acontce no site. E o melhor, este site possui um sistema de chat em tempo real para tirar dúvidas.",
         link: "#",
-        image: "/mark.png",
+        image: "/mark.svg",
         reverse: false,
     },
     {
@@ -33,7 +33,7 @@ export const portfolio = [
         name: "Jogo Quiz",
         description: "Foi desenvolvido para fins acadêmicos, é um site bem legal, onde possui um ranking e vários quizes.",
         link: "https://quiz-school-word.vercel.app/",
-        image: "/quiz_my.png",
+        image: "/quiz_my.svg",
         reverse: true,
     },
     {
@@ -42,7 +42,7 @@ export const portfolio = [
         name: "Jogo Quiz",
         description: "Esse outro quiz foi desenvolvido a muito tempo, em uma semana gratuita de aula da alura. Portanto eu modifiquei muitas coisas, como as perguntas estarem salvas no banco de dados e um rankin ordenados.",
         link: "https://quiz-two-indol.vercel.app/",
-        image: "/quiz.png",
+        image: "/quiz.svg",
         reverse: false,
     },
     {
@@ -51,7 +51,7 @@ export const portfolio = [
         name: "Site de demonstração de um restaurante",
         description: "Este site também possui muito tempo que desenvolvi, ele tem a finalidade de ser como um cardápio e também possui um painel de controle para gerenciar praticamente tudo do site.",
         link: "https://fronted-delivery.vercel.app/",
-        image: "/hamb.png",
+        image: "/hamb.svg",
         reverse: true,
     },
     {
@@ -60,7 +60,7 @@ export const portfolio = [
         name: "Chat em tempo real",
         link: "#",
         description: "Este aplicativo foi desenvolvido para ser usado apenas por uma pessoa. Ele possui um sistema de chat em tempo real para conversarem com pessoas que entram em contato pelo site dele.",
-        image: "/app.png",
+        image: "/app.svg",
         reverse: false,
     },
 ];
